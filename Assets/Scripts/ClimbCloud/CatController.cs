077b859c444916d41edaa1c646a5ff35b7be21cf
S39 <stellagalaxy39@protonmail.com>
2024-01-31 15:16:38 +0900

    ClimbClould : 고양이 움직이기 & 방향전환

--- Assets/Scripts/ClimbCloud/CatController.cs
+++ Assets/Scripts/ClimbCloud/CatController.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using UnityEditor.Experimental.GraphView;
 using UnityEngine;
 
 public class CatController : MonoBehaviour
@@ -9,26 +10,52 @@ public class CatController : MonoBehaviour
     [SerializeField] private Rigidbody2D rbody;
     [SerializeField] private float jumpForce = 680f;
     [SerializeField] private float moveForce = 500f;
+    private ClimbClouldGameDirector gameDirector;
+
+    private void Start() 
+    {
+        this.gameDirector = GameObject.FindAnyObjectByType<ClimbClouldGameDirector>();
+    }
     
     void Update()
     {
         if(Input.GetKeyDown(KeyCode.Space))
         {
             // 점프
-            this.rbody.AddForce(this.transform.up * this.jumpForce); // 로컬좌표와 유사
             // this.rbody.AddForce(Vector3.up * this.force); // 월드좌표상            
+            this.rbody.AddForce(this.transform.up * this.jumpForce); // 로컬좌표와 유사
         }
 
-        if(Input.GetKeyDown(KeyCode.LeftArrow))
+        int dir = 0;
+
+
+        if(Input.GetKey(KeyCode.LeftArrow))
         {
-            this.rbody.AddForce(-(this.transform.right) * this.moveForce);
+            dir = -1;
         }
 
-        if(Input.GetKeyDown(KeyCode.RightArrow))
+        if(Input.GetKey(KeyCode.RightArrow))
         {
-            this.rbody.AddForce(this.transform.right * this.moveForce);
+            dir = 1;
         }
 
+        ReversCat(dir); // 회전
 
+        if(Mathf.Abs(this.rbody.velocity.x) < 3)
+        {
+            this.rbody.AddForce((this.transform.right*dir) * this.moveForce);
+        }
+
+        this.gameDirector.UpdateVelocityText(this.rbody.velocity);
+
+
+    }
+
+    private void ReversCat(int dir)
+    {
+        if(dir != 0)
+        {
+            this.transform.localScale = new Vector3(dir, 1, 1);
+        }
     }
 }
